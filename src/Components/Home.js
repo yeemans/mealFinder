bc01de0ed0f0e4d14e8de23d90114fe87db61a8b
yeemans <arthur.roth.07@gmail.com>
2023-04-25 14:22:52 -0700

    added support for diets

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,15 @@
 import {useState, useEffect} from "react"
+// import Select from 'react-select';
 import axios from "axios"
 
 function Home() { 
     const searchURL = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}`;
     const [searchTerm, setSearchTerm] = useState("");
     const [cuisines, setCuisines] = useState(new Set());
+    const [diets, setDiets] = useState(new Set());
+
+    const dietOptions = ["balanced", "high-fiber", "high-protein", "low-carb", "low-fat", "low-sodium"];
+        
 
     function setSearch(e) { 
         let term = "";
@@ -14,22 +19,36 @@ function Home() {
     }
 
     function addCuisine(e) { 
-        const clone = new Set(cuisines);
-        // if cuisine is already toggled on
-        if (clone.has(e.target.value)) 
-            clone.delete(e.target.value);
-        else 
-            clone.add(e.target.value); 
-
+        let clone = new Set(cuisines);
+        clone = addToggleable(clone, e.target.value)
         setCuisines(clone); 
         console.log(clone);
+    }
+
+    function addDiet(e) { 
+        let clone = new Set(diets); 
+        clone = addToggleable(clone, e.target.value); 
+        setDiets(clone); 
+        console.log(clone);
+    }
+
+    function addToggleable(items, item) { 
+        if (items.has(item)) 
+            items.delete(item) 
+        else 
+            items.add(item); 
+        return items;
     }
 
     function getSearchURL() { 
         // add search term, cuisine, and more to do at the end of searchURL 
         let url = searchURL;
+        
         for (let cuisine of cuisines) 
-            url += `&cuisineType=${cuisine}`
+            url += `&cuisineType=${cuisine}`;
+        for (let diet of diets) 
+            url += `&diet=${diet}`;
+
         return url + searchTerm;
     }
 
@@ -49,15 +68,27 @@ function Home() {
 
             <h2>Cuisines: All cuisines are included by default</h2>
             <input type="checkbox" id="American" onChange={(e) => addCuisine(e)} value="American" />
-            <label for="American">American</label>
+            <label htmlFor="American">American</label>
 
             <input type="checkbox" id="Asian" onChange={(e) => addCuisine(e)} value="Asian" />
-            <label for="Asian">Asian</label>
+            <label htmlFor="Asian">Asian</label>
 
             <input type="checkbox" id="Mexican" onChange={(e) => addCuisine(e)} value="Mexican" />
-            <label for="Mexican">Mexican</label>
+            <label htmlFor="Mexican">Mexican</label>
+
+            <h2>Diets</h2>
+            {dietOptions.map(diet => { 
+                return(
+                    <div> 
+                        <label htmlFor={diet}>{diet}</label>
+                        <input type="checkbox" value={diet} key={diet} id={diet} onChange=
+                            {(e) => addDiet(e) }/>
+                    </div>
+                )
+            })}
 
             <button onClick={() => findRecipes()}>Find Recipes</button>
+
         </div>
     )
 }
